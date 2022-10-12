853ae8b1b86daa1515d956374d3215edeb820974
Bogdan Pizhuk <bogdanpizuk2604@gmail.com>
2022-10-12 12:44:37 +0300

    +corsMiddleware

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from uvicorn import run as startserver
+from fastapi.middleware.cors import CORSMiddleware
 from db import postgres_engine, redis_engine
-app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/', status_code=200)
 async def root():
@@ -16,4 +28,4 @@ def health_check():
 app.add_api_route('/health', endpoint=health_check)
 
 if __name__ == '__main__':
-    startserver(app, host="0.0.0.0", port=8000)
+    startserver('main:app', host="0.0.0.0", port=8000, reload=True, reload_dirs=['/src'])
